4dca2ee99e6c9728f54f6b51d6a5aee28318eb0f
ViniVentura94 <viniventura94@gmail.com>
2025-05-24 16:22:56 -0300

    Adicionado Banner Dia dos Namorados e aplicado na Home.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@
 import { Hero, Section } from './styles'
 import { SectionTitle } from '../../components/SectionTitle'
 import { ProductCarousel } from '../../components/ProductCarousel'
-import heroBackground from '../../assets/banners/BannerMayyaClubLancamento.jpg'
+import heroBackground from '../../assets/banners/BannerMayyaClubDiaNamorados.jpg'
 
 
 export function Home() {
@@ -10,7 +10,7 @@ export function Home() {
     return (
         <>
             <Hero>
-                <img src={heroBackground} alt="Lançamento Mayya Club - 20/05/2025" />
+                <img src={heroBackground} alt="Promoção Mayya Club - Dia dos Namorados" />
             </Hero>
 
             <Section id="catalogo">
